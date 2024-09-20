425e20aa8bfc0f791f4465a7f7f8a082fdd9fb2e
“Justin” <jdenton.bills@gmail.com>
2024-09-20 16:36:22 -0500

    Stub out functional ul in componenthtml

--- ngExpense/src/app/components/home/home.component.ts
+++ ngExpense/src/app/components/home/home.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ExpenseService } from '../../services/expense.service';
+import { Expense } from '../../models/expense';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  expenses: Expense[] = [];
+
+  constructor(
+    private expenseService: ExpenseService
+  ){}
+
+  ngOnInit(): void {
+    this.reloadExpenses();
+  }
+
+  reloadExpenses(): void {
+    this.expenseService.index().subscribe({
+      next: (expenseList) => {
+        this.expenses = expenseList;
+      },
+      error: (fail) => {
+        console.error('error retrieving expense list');
+        console.error(fail);
+      }
+    });
+  }
+
 
 }
--- ngExpense/src/app/services/expense.service.ts
+++ ngExpense/src/app/services/expense.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Expense } from '../models/expense';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpenseService {
 
-  constructor() { }
+  url = environment.baseUrl + "api/expenses";
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  index(): Observable<Expense[]> {
+    return this.http.get<Expense[]>(this.url).pipe(
+      catchError(
+        (err: any) => {
+          console.log(err);
+          return throwError(
+            () => new Error('expense.service.ts - index() : error retrieving expense: ' + err)
+          );
+        }
+      )
+    );
+  }
+
+  // create(expense: Expense): Observable<Expense> {
+  //   return this.http.post<Expense>(this.url, expense).pipe(
+  //     catchError(
+  //       (err: any) => {
+  //         console.log(err);
+  //         return throwError(
+  //           () => new Error('expense.service.ts - create() : error creating expense: ' + err)
+  //         );
+  //       }
+  //     )
+  //   );
+  // }
+
+
 }
